4886b107eb1b5ac1bc0e80420b0915020e783a00
Tien Thanh <tienthanh8490@gmail.com>
2014-08-03 14:33:07 +0800

    relational join using MapReduce

--- assignment3/join.py
+++ assignment3/join.py
@@ -14,19 +14,32 @@ def mapper(record):
     # item_type: order or line item
     # item_id: order_id
     item_type = record[0]
-    item_id = record[1]    
-    for w in words:
-      mr.emit_intermediate(w, key)
+    item_id = record[1]          
+    
+    mr.emit_intermediate(item_id, record)
 
-def reducer(key, list_of_values):
-    # key: word
-    # value: list of occurrence counts
-    total = []
-    for v in list_of_values:
-      if v not in total:
-        total.append(v)
-    mr.emit((key, total))
+def reducer(key, list_of_values):    
 
+    single_record = []
+    list_record = []
+
+    order_item = []
+
+    for v in list_of_values:           
+        
+      if v[0]=='order':
+        order_item = v
+
+      if 'order' not in single_record:
+        single_record += order_item
+
+      if v[0]=='line_item':
+        single_record += v
+
+      if 'order' in single_record and 'line_item' in single_record:
+        mr.emit(single_record)
+        single_record = []
+      
 # Do not modify below this line
 # =============================
 if __name__ == '__main__':
